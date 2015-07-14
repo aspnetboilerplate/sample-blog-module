196289b1941b3dfebc8ad660afa4d2abbca5a706
hikalkan <hi_kalkan@yahoo.com>
2015-07-14 10:26:56 +0300

    Added initial project.

---
+++ src/Abp.Samples.Blog.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.Samples.Blog.Core
+{
+    public class Class1
+    {
+    }
+}
