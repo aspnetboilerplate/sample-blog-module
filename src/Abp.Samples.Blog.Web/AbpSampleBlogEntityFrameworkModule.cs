8c4dc565d97f6d0219cef3ab6965af500bcb56b2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-07-15 23:07:15 +0300

    Added web module.

---
+++ src/Abp.Samples.Blog.Web/AbpSampleBlogEntityFrameworkModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Abp.Application.Services;
+using Abp.Modules;
+using Abp.Samples.Blog.Posts;
+using Abp.Web.Mvc;
+using Abp.WebApi;
+using Abp.WebApi.Controllers.Dynamic.Builders;
+
+namespace Abp.Samples.Blog.Web
+{
+    [DependsOn(typeof(AbpSampleBlogApplicationModule), typeof(AbpWebApiModule), typeof(AbpWebMvcModule))]
+    public class AbpSampleBlogEntityFrameworkModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+            DynamicApiControllerBuilder.ForAll<IApplicationService>(typeof (PostAppService).Assembly, "blog");
+        }
+    }
+}
