c41ff0a537eb84272f47c2d7c5d3536a3204976e
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-07-16 11:44:30 +0300

    Added blog view and controller

---
+++ app/MyAbpZeroProject.Web/App/Main/views/blog/blog.js
@@ -0,0 +1,29 @@
+﻿(function() {
+
+    angular.module('app').config([
+        '$stateProvider',
+        function ($stateProvider) {
+            $stateProvider
+                .state('blog', {
+                    url: '/blog',
+                    templateUrl: '/App/Main/views/blog/blog.cshtml',
+                    menu: 'SampleBlog.AdminPage'
+                });
+        }
+    ]);
+
+    angular.module('app').controller('app.views.blog', [
+        '$scope', 'abp.services.blog.post',
+        function ($scope, postService) {
+            var vm = this;
+
+            vm.posts = [];
+
+            postService.getPosts({
+                maxResultCount: 1000
+            }).success(function(result) {
+                vm.posts = result.items;
+            });
+        }
+    ]);
+})();
