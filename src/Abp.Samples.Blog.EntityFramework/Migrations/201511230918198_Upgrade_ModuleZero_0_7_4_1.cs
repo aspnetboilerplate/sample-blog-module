3ef8bbb1b44ffaa6659f173707663ec9863e6eb0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-11-23 11:19:28 +0200

    Added missing migration.

---
+++ src/Abp.Samples.Blog.EntityFramework/Migrations/201511230918198_Upgrade_ModuleZero_0_7_4_1.cs
@@ -0,0 +1,20 @@
+namespace Abp.Samples.Blog.EntityFramework.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure.Annotations;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Upgrade_ModuleZero_0_7_4_1 : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
