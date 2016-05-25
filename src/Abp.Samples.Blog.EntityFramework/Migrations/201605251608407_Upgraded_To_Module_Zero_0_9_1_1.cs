8d0a0ad17e9d211a62573574aea2dedc26cbde84
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-05-25 19:09:32 +0300

    Added migration to upgrade to module zero 0.9.1.1.

---
+++ src/Abp.Samples.Blog.EntityFramework/Migrations/201605251608407_Upgraded_To_Module_Zero_0_9_1_1.cs
@@ -0,0 +1,18 @@
+namespace Abp.Samples.Blog.EntityFramework.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure.Annotations;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Upgraded_To_Module_Zero_0_9_1_1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
