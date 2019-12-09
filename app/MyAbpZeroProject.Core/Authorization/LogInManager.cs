c4c6a700ddc9ba9c3541f4e0dc5ecc89e8823c95
Musa Demir <musa.demir@volosoft.com>
2019-12-09 18:12:13 +0300

    fix problems

---
+++ app/MyAbpZeroProject.Core/Authorization/LogInManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Authorization;
+using Abp.Authorization.Roles;
+using Abp.Authorization.Users;
+using Abp.Configuration;
+using Abp.Configuration.Startup;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.Zero.Configuration;
+using MyAbpZeroProject.Authorization.Roles;
+using MyAbpZeroProject.MultiTenancy;
+using MyAbpZeroProject.Users;
+
+namespace MyAbpZeroProject.Authorization
+{
+    public class LogInManager : AbpLogInManager<Tenant, Role, User>
+    {
+        public LogInManager(
+            UserManager userManager,
+            IMultiTenancyConfig multiTenancyConfig,
+            IRepository<Tenant> tenantRepository,
+            IUnitOfWorkManager unitOfWorkManager,
+            ISettingManager settingManager,
+            IRepository<UserLoginAttempt, long> userLoginAttemptRepository,
+            IUserManagementConfig userManagementConfig,
+            IIocResolver iocResolver,
+            RoleManager roleManager) 
+            : base(userManager, multiTenancyConfig, tenantRepository, unitOfWorkManager, settingManager, userLoginAttemptRepository, userManagementConfig, iocResolver, roleManager)
+        {
+        }
+    }
+}
